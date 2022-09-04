e8ff1a58867c7943c106a1e02810420fcb4d1e3e
Mateo Miniati <71853824+miniatimat@users.noreply.github.com>
2022-09-04 16:45:26 -0300

    Delete api directory

--- api/src/routes/index.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const productRoutes = require("./productroutes");
-const categoryRoutes = require("./categoryroutes");
-
-router.use("/product", productRoutes);
-router.use("/category", categoryRoutes);
-
-module.exports = router;
